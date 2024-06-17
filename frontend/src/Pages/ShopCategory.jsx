6aea9ebad68e67385124be6982b5b3be7ab614ab
stevezhou0826 <zhousihao826@gmail.com>
2024-06-17 09:00:22 -0400

    6/17 1:30:05 maybe not working

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import "./CSS/ShopCategory.css";
 
 const ShopCategory = () => {
-  return <div></div>;
+  const { all_product } = useContext(ShopContext);
+  return <div className="shop-category"></div>;
 };
 
 export default ShopCategory;
