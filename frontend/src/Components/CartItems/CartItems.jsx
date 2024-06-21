316cec0f3daffc3c09d66d8e8beadee98e7a1284
stevezhou0826 <zhousihao826@gmail.com>
2024-06-21 04:09:48 -0400

    6/21 2:58:55 first part done

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,8 @@ import remove_icon from "../Assets/cart_cross_icon.png";
 import "./CartItems.css";
 
 const CartItems = () => {
-  const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
+  const { getTotalCartAmount, all_product, cartItems, removeFromCart } =
+    useContext(ShopContext);
 
   return (
     <div className="cartitems">
@@ -49,7 +50,7 @@ const CartItems = () => {
             <div>
               <div className="cartitems-total-item">
                 <p>Subtotal</p>
-                <p>${0}</p>
+                <p>${getTotalCartAmount()}</p>
               </div>
               <hr />
               <div className="cartitems-total-item">
@@ -59,7 +60,7 @@ const CartItems = () => {
               <hr />
               <div className="cartitems-total-item">
                 <h3>Total</h3>
-                <h3>${0}</h3>
+                <h3>${getTotalCartAmount()}</h3>
               </div>
             </div>
             <button>PROCEED TO CHECKOUT</button>
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
 import cart_icon from "../Assets/cart_icon.png";
 import logo from "../Assets/logo.png";
 import "./Navbar.css";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
-
+  const { getTotalCartItems } = useContext(ShopContext);
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -63,7 +64,7 @@ const Navbar = () => {
         <Link to="/cart">
           <img src={cart_icon} alt="" />
         </Link>
-        <div className="nav-cart-count">0</div>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
   );
