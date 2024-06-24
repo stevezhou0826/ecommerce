f2e7e560d9d200bbd38e8809b8ccec04f792414f
stevezhou0826 <zhousihao826@gmail.com>
2024-06-24 04:07:56 -0400

    6/24 5:57:00

--- backend/index.js
+++ backend/index.js
@@ -87,8 +87,17 @@ const Product = mongoose.model("Product", {
 });
 
 app.post("/addproduct", async (req, res) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  } else {
+    id = 1;
+  }
   const product = new Product({
-    id: req.body.id,
+    id: id,
     name: req.body.name,
     image: req.body.image,
     category: req.body.category,
@@ -102,6 +111,25 @@ app.post("/addproduct", async (req, res) => {
     success: true,
     name: req.body.name,
   });
+});
+
+// API for deleting products
+app.post("/removeproduct", async (req, res) => {
+  await Product.findOneAndDelete({
+    id: req.body.id,
+  });
+  console.log("Product Deleted Successfully");
+  res.json({
+    success: true,
+    name: req.body.name,
+  }); // Send a response to the client
+});
+
+// API for getting all products
+app.get("/allproducts", async (req, res) => {
+  let products = await Product.find({});
+  console.log("All Products feched successfully");
+  res.json(products);
 });
 
 app.listen(port, (error) => {
