86e37b7692e83dc0c30ebe4c6bf33672e81a3ed5
stevezhou0826 <zhousihao826@gmail.com>
2024-06-25 04:06:41 -0400

    6/25 7:00:50

---
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ListProduct.css";
+
+const ListProduct = () => {
+  return <div className="list-product">{/* Your component code here */}</div>;
+};
+
+export default ListProduct;
