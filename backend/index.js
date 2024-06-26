ae9a236e566b8115557f0c903c5a31b1309d63fa
stevezhou0826 <zhousihao826@gmail.com>
2024-06-26 03:14:43 -0400

    6/26 7:49:10

--- backend/index.js
+++ backend/index.js
@@ -86,6 +86,57 @@ const Product = mongoose.model("Product", {
   },
 });
 
+// scheme creating for user model
+const Users = mongoose.model("Users", {
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  cartData: {
+    type: Object,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Creating endpoint for registering the user
+app.post("/signup", async (req, res) => {
+  let check = await Users.findOne({ email: req.body.email });
+
+  if (check) {
+    return res.status(400).json({
+      success: false,
+      errors: "existing user found with same email address",
+    });
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+  const user = new Users({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    cartData: cart,
+  });
+
+  await user.save();
+  const data = {
+    user: { id: user.id },
+  };
+
+  const token = jwt.sign(data, "secret_ecom");
+  res.json({ success: true, token });
+});
+
 app.post("/addproduct", async (req, res) => {
   let products = await Product.find({});
   let id;
@@ -137,5 +188,26 @@ app.listen(port, (error) => {
     console.log("Server Running on Port: " + port);
   } else {
     console.log("Error: " + error);
+  }
+});
+
+// creating endpoint for user login
+app.post("/login", async (req, res) => {
+  let user = await Users.findOne({ email: req.body.email });
+  if (user) {
+    const passCompare = req.body.password === user.password;
+    if (passCompare) {
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = jwt.sign(data, "secret_ecom");
+      res.json({ sucess: true, token });
+    } else {
+      res.json({ sucess: false, errors: "wrong password" });
+    }
+  } else {
+    res.json({ sucess: false, errors: "wrong email id" });
   }
 });
